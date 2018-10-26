ff66f771e07e9584febefa7bbdee6bd7d6d0e021
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-10-26 11:44:31 -0400

    feat: Add children of props to render

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class Page extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <p>Page Component</p>
+        {this.props.children}
+      </div>
+    );
   }
 }
 
