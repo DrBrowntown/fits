10c471aa9f68ab22996de168b5ff58eafa23c344
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-01 15:37:06 -0400

    feat: Import ThemeProvider and injectGlobal

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
-import styled from "styled-components";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
 
 const StyledPage = styled.div`
   background: white;
@@ -12,7 +22,6 @@ const Inner = styled.div`
   max-width: 1000px;
   margin: 0 auto;
   padding: 2rem;
-  background: red;
 `;
 
 class Page extends Component {
