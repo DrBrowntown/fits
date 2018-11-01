2347dc952784fbbaf7f12501db6d4cf10850a458
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-01 15:18:59 -0400

    feat: Import styled, declare StyledPage, and refactor div as StyledPage

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import Meta from "../components/Meta";
+import styled from "styled-components";
+
+const StyledPage = styled.div`
+  background: white;
+  color: black;
+`;
 
 class Page extends Component {
   render() {
     return (
-      <div>
+      <StyledPage>
         <Meta />
         <Header />
         {this.props.children}
-      </div>
+      </StyledPage>
     );
   }
 }
