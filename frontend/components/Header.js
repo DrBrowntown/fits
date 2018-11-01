5db53c66bbec1a99b2c9eb761cd2df1c237e1e23
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-01 15:59:16 -0400

    feat: Define bar in StyledHeader

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -21,7 +21,19 @@ const Logo = styled.h1`
   }
 `;
 
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+`;
 
 const Header = () => (
   <StyledHeader>
