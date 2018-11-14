66166ba9156f27fe86508277d1cf80aee2834104
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-14 17:04:10 -0500

    feat: Define static getInitialProps method to check for componenents and surface the values

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,12 +4,22 @@ import { ApolloProvider } from "react-apollo";
 import withData from "../lib/withData";
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
   render() {
-    const { Component } = this.props;
+    const { Component, apollo } = this.props;
 
     return (
       <Container>
-        <ApolloProvider client={this.props.apollo}>
+        <ApolloProvider client={apollo}>
           <Page>
             <Component />
           </Page>
