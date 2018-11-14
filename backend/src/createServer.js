df2d8a84b340dfe272407839bebb8eda0939527d
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-14 14:29:58 -0500

    feat: Declare createServer and export

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -2,3 +2,13 @@ const { GraphQLServer } = require("graphql-yoga");
 const Mutation = require("./resolvers/Mutation");
 const Query = require("./resolvers/Query");
 const db = requrie("./db");
+
+// Create the GraphQL Yoga Server
+
+function createServer() {
+    return new GraphQLServer({
+        typeDefs: 
+    });
+}
+
+module.exports = createServer;
