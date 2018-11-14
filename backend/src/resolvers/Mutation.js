fbc5b066f278b109c6d91f9efafb2509d8ed75a0
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-14 16:25:43 -0500

    feat: Declare item, spread args into data object, and return item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,4 +1,18 @@
 const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+
+    return item;
+  }
   // createDog(parent, args, ctx, info) {
   //   global.dogs = global.dogs || [];
   //   //create a dog!
