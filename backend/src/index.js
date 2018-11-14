6d009c2087031f1fe513a28e1854269320e7389d
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-14 14:40:56 -0500

    feat: Start server, define frontend origin, and log port number being ran

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,3 +6,15 @@ const server = createServer();
 
 // TODO Use express middleware to handle cookies (JSONM Web Tokens)
 // TODO Use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+  }
+);
