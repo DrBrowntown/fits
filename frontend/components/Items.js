1ed7e6598b20137a829aa726e24678ffc401e602
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-15 21:07:31 -0500

    feat: Define and declare all_items_query

--- frontend/components/Items.js
+++ frontend/components/Items.js
@@ -2,6 +2,19 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
+const ALL_ITEMS_QUERY = gql`
+  query ALL_ITEMS_QUERY {
+    items {
+      id
+      title
+      price
+      description
+      image
+      largeImage
+    }
+  }
+`;
+
 class Items extends Component {
   render() {
     return (
