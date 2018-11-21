937ee167eca7026ea571feda28cfed27115c5fe7
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-20 21:41:32 -0500

    bug: Each mutation schema item needs $ in front of variable

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -5,23 +5,23 @@ import Form from "./styles/Form";
 import formatMoney from "../lib/formatMoney";
 
 const CREATE_ITEM_MUTATION = gql`
-    mutation CREATE_ITEM_MUTATION(
-        title: String!
-        description: String!
-        price: Int!
-        image: String
-        largeImage: String
+  mutation CREATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
     ) {
-        createItem(
-            title: $title
-            description: $description
-            price: $price
-            image: $image
-            largeImage: $largeImage
-        ) {
-            id
-        }
-    }    
+      id
+    }
+  }
 `;
 
 class CreateItem extends Component {
