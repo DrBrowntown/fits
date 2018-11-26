a33fcc8e575cec80913d9602ed3e226562a2c30d
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-26 17:36:48 -0500

    feat: Set state of image and Large image of the file uploaded

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -50,6 +50,12 @@ class CreateItem extends Component {
       method: 'POST',
       body: data
     });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      LargeImage:file.eager[0].secure_url
+    });
   };
   render() {
     return (
