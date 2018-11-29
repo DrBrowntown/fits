c102c82a9435165987d5da4235d7d4341f22b3fc
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-29 16:53:47 -0500

    feat: Import formatMoney function and pass price data

--- frontend/components/item.js
+++ frontend/components/item.js
@@ -4,6 +4,7 @@ import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import Link from "next/link";
+import formatMoney from "../lib/formatMoney";
 
 export default class Item extends Component {
   static propTypes = {
@@ -23,6 +24,7 @@ export default class Item extends Component {
             <a>{item.title}</a>
           </Link>
         </Title>
+        <PriceTag>{formatMoney(item.price)}</PriceTag>
       </ItemStyles>
     );
   }
