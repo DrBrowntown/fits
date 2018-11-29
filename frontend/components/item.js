f874636e9566a2ec4b239686042b712e0b60147d
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-11-29 16:50:12 -0500

    feat: Add link to item cards with a query of the item ID

--- frontend/components/item.js
+++ frontend/components/item.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
+import Link from "next/link";
 
 export default class Item extends Component {
   static propTypes = {
@@ -12,7 +13,16 @@ export default class Item extends Component {
     const { item } = this.props;
     return (
       <ItemStyles>
-        <Title>{item.title}</Title>
+        <Title>
+          <Link
+            href={{
+              pathname: "/item",
+              query: { id: item.id }
+            }}
+          >
+            <a>{item.title}</a>
+          </Link>
+        </Title>
       </ItemStyles>
     );
   }
