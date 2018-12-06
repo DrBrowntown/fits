db6826baf3fb288636275833f1d34eaa3af8464a
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-12-06 17:09:09 -0500

    feat: Remove uploadFile handler

--- frontend/components/UpdateItem.js
+++ frontend/components/UpdateItem.js
@@ -39,24 +39,6 @@ class UpdateItem extends Component {
     const val = type === "number" ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
-
-  uploadFile = async e => {
-    console.log("uploading file");
-    const files = e.target.files;
-    const data = new FormData();
-    data.append("file", files[0]);
-    data.append("upload_preset", "sickfits");
-    const res = await fetch(
-      "https://api.cloudinary.com/v1_1/drbrowntown/image/upload",
-      { method: "POST", body: data }
-    );
-    const file = await res.json();
-    console.log(file);
-    this.setState({
-      image: file.secure_url,
-      largeImage: file.eager[0].secure_url
-    });
-  };
   render() {
     return (
       <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
